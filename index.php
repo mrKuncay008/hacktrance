75c1f41b8d1a0ca6c5a0ee52829119d16844e7ed
tejho <tejho@localhost.localdomain>
2023-09-13 19:01:34 +0700

    New 13 sept

---
+++ index.php
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
+	<generator>https://textpattern.com/?v=4.8.7</generator>
+	<title>My site - Articles</title>
+	<link>http://10.1.2.133/demo/</link>
+	<atom:link href="http://10.1.2.133/demo/index.php?rss=1&amp;section=articles" rel="self" type="application/rss+xml" />
+	<description>My site slogan</description>
+	<pubDate>Tue, 02 Nov 2021 07:48:11 GMT</pubDate>
+	
+	<item>
+		<title>Welcome to Terra Vision Project</title>
+		<description><![CDATA[<p>If everything can be answered today, why is there research?</p>]]></description>
+		<link>http://10.1.2.133/demo/index.php?id=2</link>
+		<pubDate>Tue, 02 Nov 2021 07:48:00 GMT</pubDate>
+		<dc:creator>author</dc:creator>
+		<guid isPermaLink="false">tag:10.1.2.133,2021-11-02:84e31157c9858d3c7fa480a3547fd14a/89894160ad96a54ead5d56617ca8dd42</guid>
+	</item>
+</channel>
+</rss>
+<!-- Trace summary:
+Runtime   : 10.22 ms
+Query time: 5.06 ms
+Queries   : 6
+Memory (*): 601 kB
+-->
